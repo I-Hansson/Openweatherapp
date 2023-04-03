61839f7d0740156b0f2ae498463444a2f1864607
Isac Hansson <isachansson1218@gmail.com>
2023-04-03 22:17:24 +0200

    README

---
+++ weatherapp/README.mk
@@ -0,0 +1,9 @@
+# OpenWeather app
+
+## För att kunna starta programmet
+
+
+För att kunna starta programmet behöver man ladda ner [Node.js](https://nodejs.org/en/download).
+När man laddat ner Node.js måste man installera Axios, detta gör man genom att köra "npm install axios" när man är i mappen weatherapp.
+
+Sedan för att starta programmet så skall kör man "npm run dev" i weatherapp mappen. 
